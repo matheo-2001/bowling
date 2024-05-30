f7c9a83b328fc64dbac5396fb8aaf4ac80851df6
matheo-2001 <collinamatheo2001@gmail.com>
2024-05-30 16:16:15 +0200

    test push

--- src/test/kotlin/tests/LoadTest.kt
+++ src/test/kotlin/tests/LoadTest.kt
@@ -5,7 +5,6 @@ import io.gatling.javaapi.core.CoreDsl.*
 class LoadTest : BaseSimulation() {
 
     private val computerSearch = scenario("ComputerSearch").exec(search(ids))
-
     init {
         setUp(
             computerSearch.injectOpen(rampUsers(1000).during(60)),
